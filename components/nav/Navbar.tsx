51739522d931b8bb667489a79e085327810e6814
Pablo del Barrio <pablodbb@gmail.com>
2024-01-25 16:46:58 +0100

    Added icons and fixed styles in navbar

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -6,11 +6,11 @@ import { ModeToggle } from "../ToggleTheme";
 import { DialogInfo } from "../DialogInfo";
 import { useRouter } from "next/navigation";
 import { MdLogout } from "react-icons/md";
+import { GrAddCircle } from "react-icons/gr";
 import createSupabaseFrontendClient from "@/lib/supabase/supabase";
 
 export default function Navbar() {
   const [signOutButton, setSignOutButton] = useState(false);
-  // const [session, setSession] = useState(null);
 
   const supabase = createSupabaseFrontendClient();
   const router = useRouter();
@@ -41,11 +41,7 @@ export default function Navbar() {
   };
 
   return (
-    // <nav className="max-w-xl mx-auto p-2 md:max-w-1/2">
     <nav className="max-w-xl mx-auto p-2 md:max-w-1/2">
-      {/* <div className="h-[120px]">
-        <div className="flex justify-between items-center mt-2">
-          <div className="flex"> */}
       <div className="flex flex-col justify-start h-[120px]">
         <div className="flex justify-between mt-2">
           <div className="flex flex-col">
@@ -62,16 +58,17 @@ export default function Navbar() {
           <div className="flex flex-col">
             <Link
               href="/add-event"
-              className="bg-card text-background font-bold px-4 py-1 rounded-lg text-base"
+              className="flex flex-row gap-2 items-center justify-center bg-card text-btvgray font-bold px-4 py-1 rounded-lg text-base"
             >
-              add event
+              afegeix
+              <GrAddCircle />
             </Link>
             {signOutButton && (
               <button
                 onClick={handleSignout}
-                className="flex flex-row gap-2 items-center justify-center bg-btvgray dark:bg-glow text-white dark:text-black text-xs p-2 px-2 mt-2 rounded-lg"
+                className="flex flex-row gap-2 items-center justify-center bg-btvgray dark:bg-glow font-bold text-white dark:text-black text-xs p-2 px-2 mt-2 rounded-lg"
               >
-                SIGN OUT
+                SURT
                 <MdLogout />
               </button>
             )}
