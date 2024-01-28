9812a83829e47e3d2a6e99f68a9242ae5bde17c6
Pablo del Barrio <pablodbb@gmail.com>
2024-01-28 20:42:17 +0100

    fixing supabase server error

--- lib/actions/events.ts
+++ lib/actions/events.ts
@@ -1,11 +1,13 @@
+"use server";
+import { createSupabaseServerClient } from "../supabase";
 import { createServerClient } from "../supabase/supabaseServerClient";
 
-const supabase = createServerClient();
+const supabase = createSupabaseServerClient();
 // export const revalidate = 60;
 
 export async function getEvents() {
   const currentDate = new Date();
-  const { data, error } = await supabase
+  const { data, error } = await (await supabase)
     .from("events")
     .select("*")
     .gt("date", currentDate.toISOString())
