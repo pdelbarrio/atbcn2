0f1176443fed7e401ac646cad97c9682a4742094
Pablo del Barrio <pablodbb@gmail.com>
2024-05-18 15:50:58 +0200

    fixing dependency errors

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} = require("next/constants");
-
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -15,32 +11,4 @@ const nextConfig = {
   },
 };
 
-/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
-module.exports = async (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withSerwist = (await import("@serwist/next")).default({
-      // Note: This is only an example. If you use Pages Router,
-      // use something else that works, such as "service-worker/index.ts".
-      swSrc: "app/sw.ts",
-      swDest: "public/sw.js",
-    });
-    return withSerwist(nextConfig);
-  }
-
-  return nextConfig;
-};
-
-// /** @type {import('next').NextConfig} */
-
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         hostname: "res.cloudinary.com",
-//         protocol: "https",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
+module.exports = nextConfig;
