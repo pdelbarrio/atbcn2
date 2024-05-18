f7ec1d1527297be165fcd69be15270dc16bb1293
Pablo del Barrio <pablodbb@gmail.com>
2024-05-18 17:32:28 +0200

    pwa testing

--- next.config.js
+++ next.config.js
@@ -12,3 +12,35 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+//FIXME:1
+// const withPWA = require("next-pwa");
+
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         hostname: "res.cloudinary.com",
+//         protocol: "https",
+//       },
+//     ],
+//   },
+//   pwa: {
+//     dest: "public",
+//     // other pwa configuration options go here
+//   },
+// };
+
+// const withPWA = require("next-pwa");
+
+// const nextConfig = {
+//   pwa: {
+//     dest: "public",
+//     // other pwa configuration options go here
+//   },
+//   images: {
+//     domains: ["res.cloudinary.com"],
+//   },
+// };
+
+// module.exports = withPWA(nextConfig);
